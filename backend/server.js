37f20c30d22f0d9b2e738550bf1ad5f0eac3a11c
mahalakshmi <banalaveeramahalakshmi20809@gmail.com>
2023-10-26 10:27:38 +0530

    added the widgtes and charts

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 4000; // Change this to your desired port
+const path = require("path");
+
+// Serve static files, including your CSS, "widget.html," and "chart1.html"
+app.use(express.static(path.join(__dirname)));
 
 // Parse JSON requests
 app.use(bodyParser.json());
 
-// Define an endpoint to toggle the device (this is just a placeholder)
-app.post("/toggle-device", (req, res) => {
-  // Implement device control logic here
-  // You can access the request body using req.body
-  // For example, if you want to toggle the device based on a command, you can do:
-  console.log(req.body.command);
-  const command = req.body.command;
-  // Implement device control logic here based on the command
-
-  // Respond with a success message
-  res.status(200).json({
-    result: command,
-    message: "Device toggled successfully",
-  });
+// Define an endpoint to serve the index.html file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
